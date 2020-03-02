bb04c347793350a3bf63d10ad31be423820f8f0a
Euller Cristian <eullercdr@gmail.com>
2020-03-02 08:28:33 -0300

    Configuracoes eslint hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,8 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier'
+    'prettier',
+    'react-hooks'
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -31,6 +32,12 @@ module.exports = {
       'warn',
       { extensions: ['.jsx','.js']}
     ],
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'no-console': ['error', { allow: ['tron']}],
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+
   },
 };
